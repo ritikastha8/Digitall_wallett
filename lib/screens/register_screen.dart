b2ce2318f5ab5b65e0d29d0f3a0b23bb967d9585
Ritika Shrestha <shrestharitika998@gmail.com>
2025-12-01 22:31:36 +0545

    Initial commit of register screen

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,1 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _mobileController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _mobileController.dispose();
+    _passwordController.dispose();
+    _confirmController.dispose();
+    super.dispose();
+  }
+
+  void _onRegisterPressed() {}
+
+  @override
+  Widget build(BuildContext context) {
+    const orange = Color(0xFFD87920);
+
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 32),
+            child: Column(mainAxisSize: MainAxisSize.min, children: const []),
+          ),
+        ),
+      ),
+    );
+  }
+}
