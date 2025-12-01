b1b526f934eacc3f6c7101d95c729560346ae2b7
Ritika Shrestha <shrestharitika998@gmail.com>
2025-12-01 22:47:05 +0545

    Changes in app dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,15 @@
 import 'package:digital_wallett_system/screens/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:digital_wallett_system/screens/register_screen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: LoginScreen());
+    return MaterialApp(
+      home: LoginScreen(),
+      routes: {'/register': (context) => const RegisterScreen()},
+    );
   }
 }
