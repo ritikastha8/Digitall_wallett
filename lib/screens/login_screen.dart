580f9ac23856a24d2c96f9f3d2173318b4a94f6b
Ritika Shrestha <shrestharitika998@gmail.com>
2025-12-01 22:22:09 +0545

    Added title and logo

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -29,12 +29,32 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
+    const orange = Color(0xFFD87920);
     return Scaffold(
       body: SafeArea(
         child: Center(
           child: SingleChildScrollView(
             padding: const EdgeInsets.symmetric(horizontal: 32),
-            child: Column(mainAxisSize: MainAxisSize.min, children: const []),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const SizedBox(height: 16),
+                const Text(
+                  'Log In',
+                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 36),
+                Container(
+                  padding: const EdgeInsets.only(top: 30),
+                  alignment: Alignment.center,
+                  child: SvgPicture.asset(
+                    "assets/images/logonovacash.svg",
+                    height: 150,
+                  ),
+                ),
+                const SizedBox(height: 48),
+              ],
+            ),
           ),
         ),
       ),
