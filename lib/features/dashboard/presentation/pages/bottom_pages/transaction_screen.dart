9bb768c8f22a7e0ecc954d598280f4c386dbaa08
Ritika Shrestha <shrestharitika998@gmail.com>
2026-01-03 00:09:55 +0545

    Added bottom pages profile and transaction

---
+++ lib/features/dashboard/presentation/pages/bottom_pages/transaction_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TransactionScreen extends StatelessWidget {
+  const TransactionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(child: Center(child: Text('Transaction Screen')));
+  }
+}
