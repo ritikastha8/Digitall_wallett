4e0de1aee8a3cfeda1d66badd79249e0ae3cba0b
Ritika Shrestha <shrestharitika998@gmail.com>
2026-01-03 00:00:28 +0545

    Added auth usecase login

---
+++ lib/features/auth/domain/usecases/login_usecase.dart
@@ -0,0 +1,44 @@
+import 'package:dartz/dartz.dart';
+import 'package:digital_wallett_system/core/errors/failures.dart';
+import 'package:digital_wallett_system/core/usecases/app_usecase.dart';
+import 'package:digital_wallett_system/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:digital_wallett_system/features/auth/domain/entities/auth_entity.dart';
+import 'package:digital_wallett_system/features/auth/domain/repositories/auth_repository.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// Params
+class LoginUsecaseParams extends Equatable {
+  final String mobileNumber;
+  final String password;
+
+  const LoginUsecaseParams({
+    required this.mobileNumber,
+    required this.password,
+  });
+
+  @override
+  List<Object?> get props => [mobileNumber, password];
+}
+
+// Provider
+
+final loginUsecaseProvider = Provider<LoginUsecase>((ref) {
+  final authRepository = ref.read(authRepositoryProvider);
+  return LoginUsecase(authRepository: authRepository);
+});
+
+// Usecase
+
+class LoginUsecase
+    implements UsecaseWithParams<AuthEntity, LoginUsecaseParams> {
+  final IAuthRepository _authRepository;
+
+  LoginUsecase({required IAuthRepository authRepository})
+    : _authRepository = authRepository;
+
+  @override
+  Future<Either<Failure, AuthEntity>> call(LoginUsecaseParams params) {
+    return _authRepository.login(params.mobileNumber, params.password);
+  }
+}
