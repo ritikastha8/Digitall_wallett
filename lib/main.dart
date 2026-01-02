40c85ab9fe848fe1a2a23ed8c70d5dcfddbf3a6c
Ritika Shrestha <shrestharitika998@gmail.com>
2026-01-02 23:31:03 +0545

    Updated main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,24 @@
-import 'package:digital_wallett_system/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:digital_wallett_system/app/app.dart';
+import 'package:digital_wallett_system/core/services/hive/hive_service.dart';
 
-void main() {
-  runApp(const App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // System UI styling (same idea as Lost & Found)
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ),
+  );
+
+  // Initialize Hive
+  await HiveService().init();
+
+  runApp(const ProviderScope(child: MyApp()));
 }
