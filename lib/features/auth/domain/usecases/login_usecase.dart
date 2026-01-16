7201198fb5e025d1a49d76033cdcd546dc7fdbbd
Ritika Shrestha <shrestharitika998@gmail.com>
2026-01-16 16:12:07 +0545

    login usecase updated

--- lib/features/auth/domain/usecases/login_usecase.dart
+++ lib/features/auth/domain/usecases/login_usecase.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:digital_wallett_system/core/errors/failures.dart';
 import 'package:digital_wallett_system/core/usecases/app_usecase.dart';
-import 'package:digital_wallett_system/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:digital_wallett_system/features/auth/data/repositories/auth_repository.dart';
 import 'package:digital_wallett_system/features/auth/domain/entities/auth_entity.dart';
 import 'package:digital_wallett_system/features/auth/domain/repositories/auth_repository.dart';
 import 'package:equatable/equatable.dart';
