a43831fcfbf9b33b1ce0a9577eb032d6750b40a2
Ritika Shrestha <shrestharitika998@gmail.com>
2026-01-30 22:34:26 +0545

    Added logout usecase

---
+++ lib/features/auth/domain/usecases/logout_usecase.dart
@@ -0,0 +1,24 @@
+import 'package:dartz/dartz.dart';
+import 'package:digital_wallett_system/core/errors/failures.dart';
+import 'package:digital_wallett_system/core/usecases/app_usecase.dart';
+import 'package:digital_wallett_system/features/auth/data/repositories/auth_repository.dart';
+import 'package:digital_wallett_system/features/auth/domain/repositories/auth_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// Create Provider
+final logoutUsecaseProvider = Provider<LogoutUsecase>((ref) {
+  final authRepository = ref.read(authRepositoryProvider);
+  return LogoutUsecase(authRepository: authRepository);
+});
+
+class LogoutUsecase implements UsecaseWithoutParams<bool> {
+  final IAuthRepository _authRepository;
+
+  LogoutUsecase({required IAuthRepository authRepository})
+    : _authRepository = authRepository;
+
+  @override
+  Future<Either<Failure, bool>> call() {
+    return _authRepository.logout();
+  }
+}
