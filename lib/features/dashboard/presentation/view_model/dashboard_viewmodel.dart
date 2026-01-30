f978a07d58105926a0bd72c01c2a08d5667f5186
Ritika Shrestha <shrestharitika998@gmail.com>
2026-01-30 22:39:06 +0545

    Added dashboard view model

---
+++ lib/features/dashboard/presentation/view_model/dashboard_viewmodel.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+import 'package:digital_wallett_system/features/dashboard/domain/usecases/upload_photo_usecase.dart';
+import 'package:digital_wallett_system/features/dashboard/presentation/state/dashboard_state.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final dashboardViewModelProvider =
+    NotifierProvider<DashboardViewModel, DashboardState>(
+      DashboardViewModel.new,
+    );
+
+class DashboardViewModel extends Notifier<DashboardState> {
+  late final uploadPhotoUsecase _uploadPhotoUsecase;
+
+  @override
+  DashboardState build() {
+    _uploadPhotoUsecase = ref.read(uploadPhotoUsecaseProvider);
+    return const DashboardState();
+  }
+
+  // upload photo
+  Future<void> uploadPhoto(File photo) async {
+    state = state.copyWith(status: DashboardStatus.loading);
+    final result = await _uploadPhotoUsecase(photo);
+    result.fold(
+      (failure) {
+        state = state.copyWith(
+          status: DashboardStatus.error,
+          errorMessage: failure.message,
+        );
+      },
+      (imageName) {
+        state = state.copyWith(
+          status: DashboardStatus.loaded,
+          uploadPhotoName: imageName,
+        );
+      },
+    );
+  }
+
+  void clearError() {
+    state = state.copyWith(errorMessage: null);
+  }
+}
